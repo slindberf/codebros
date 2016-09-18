eb2753af64b19917fa4d138781a2de5b77014633
slindberf <saralfontess@gmail.com>
2016-09-18 19:29:12 +0200

    model & controller user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/show'
+
+  get 'users/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
