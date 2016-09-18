f8c72f58aa8c26505ef8194dfffb634800908db8
slindberf <saralfontess@gmail.com>
2016-09-18 21:11:18 +0200

    add foreign key to projects table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160918182154) do
+ActiveRecord::Schema.define(version: 20160918190738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20160918182154) do
     t.text     "description"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_projects_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,4 +53,5 @@ ActiveRecord::Schema.define(version: 20160918182154) do
 
   add_foreign_key "members", "projects"
   add_foreign_key "members", "users"
+  add_foreign_key "projects", "users"
 end
