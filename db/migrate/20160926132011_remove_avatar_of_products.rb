409c30fc5477fcf425bb3bab84c654550187c27b
slindberf <saralfontess@gmail.com>
2016-09-26 15:34:16 +0200

    Deleted paperclip migrations

--- db/migrate/20160926132011_remove_avatar_of_products.rb
+++
@@ -1,8 +0,0 @@
-class RemoveAvatarOfProducts < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :projects, :avatar_file_name, :string
-    remove_column :projects, :avatar_content_type, :string
-    remove_column :projects, :avatar_file_size, :string
-    remove_column :projects, :avatar_updated_at, :datetime
-  end
-end
