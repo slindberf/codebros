9f9b6e112c33c93f6954365058fd0b7b95f497ff
slindberf <saralfontess@gmail.com>
2016-09-26 10:24:23 +0200

    Projects avatar atempt

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,8 @@ class Project < ApplicationRecord
 	has_many :members, dependent: :destroy
 	has_many :users, through: :members
 
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   # accepts_nested_attributes_for :members
 
   def add_user user
